fef974be56923fb78010d9d951e03b4671a58349
Arne Bahlo <hallo@arne.me>
2014-03-19 22:37:44 +0100

    Add then

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ git pull
 function doIt() {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude ".osx" --exclude "theme.png" -av . ~
 
-    if [ "$(uname -s)" == "Darwin" ]
+    if [ "$(uname -s)" == "Darwin" ]; then
       brew bundle
     fi
 }
