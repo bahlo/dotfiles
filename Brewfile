105d471d529955d1fd58a8f8da9af02ec3d8f53e
Arne Bahlo <hallo@arne.me>
2014-08-28 16:00:27 +0200

    Add git.zsh for more alias magic

--- Brewfile
+++ Brewfile
@@ -8,12 +8,7 @@ update
 install git
 install zsh
 install htop
-install tree
 install ag
-install hub
-install wget
-install cowsay
-install ngrep
 install tmux
 install reattach-to-user-namespace
 
---
+++ zsh/git.zsh
@@ -0,0 +1,11 @@
+# For more information to the git aliases, look into .gitconfig
+alias gca="git ca"
+alias gcaa="git caa"
+alias gco="git co"
+alias gl="git l"
+alias gps="git push"
+alias gpsu"git psu"
+alias gs="git s"
+alias gst="git st"
+alias gstp="git stp"
+alias gu="git up"
