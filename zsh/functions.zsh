5ea8ef80cdb5e9955a817b01d11bb8266b497b4f
Arne Bahlo <hallo@arne.me>
2014-12-10 13:50:06 +0100

    Load vim plugins on demand <3

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -9,7 +9,7 @@ function greph () { history 0 | grep -i $1 }
 # 'work on', via https://coderwall.com/p/feoi0a
 function wo() {
   CODE_DIR=~/Code
-  cd $(find $CODE_DIR -type d -maxdepth 3 | grep -i $* | grep -Ev Pods --max-count=1)
+  cd $(find $CODE_DIR \( -type l -o -type d \) -maxdepth 3 | grep -i $* | grep -Ev Pods --max-count=1)
 }
 
 # From http://dotfiles.org/~_why/.zshrc
