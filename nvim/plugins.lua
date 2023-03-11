26533be037839a4225648101c4611edb7e509460
Arne Bahlo <hey@arne.me>
2023-03-11 13:43:00 +0100

    Add copilot to neovim

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -75,6 +75,8 @@ require('packer').startup(function(use)
   use 'hrsh7th/cmp-vsnip'
   use 'hrsh7th/vim-vsnip'                             
 
+  use 'github/copilot.vim'
+
   -- LSP
   use {
     'neovim/nvim-lspconfig',
