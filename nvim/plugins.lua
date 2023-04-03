435dc649605177f68515034ddb130184f15b5406
Arne Bahlo <hey@arne.me>
2023-04-03 09:58:06 +0200

    Add prettier

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -83,6 +83,43 @@ require('packer').startup(function(use)
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim'
   }
+  use {
+    'jose-elias-alvarez/null-ls.nvim',
+    config = function()
+      local null_ls = require("null-ls")
+
+      local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+      local event = "BufWritePre"
+      local async = event == "BufWritePost"
+
+      null_ls.setup({
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.keymap.set("n", "<Leader>f", function()
+              vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+            end, { buffer = bufnr, desc = "[lsp] format" })
+
+            -- format on save
+            vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+            vim.api.nvim_create_autocmd(event, {
+              buffer = bufnr,
+              group = group,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr, async = async })
+              end,
+              desc = "[lsp] format on save",
+            })
+          end
+
+          if client.supports_method("textDocument/rangeFormatting") then
+            vim.keymap.set("x", "<Leader>f", function()
+              vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+            end, { buffer = bufnr, desc = "[lsp] format" })
+          end
+        end,
+      })
+    end
+  }
 
   -- Git
   use { 
@@ -268,6 +305,12 @@ require('packer').startup(function(use)
         end,
         group = format_sync_grp,
       })
+    end
+  }
+  use {
+    'MunifTanjim/prettier.nvim',
+    config = function() 
+      require('prettier').setup {}
     end
   }
 end)
