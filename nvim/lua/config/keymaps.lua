f1fb2f601b15929e30e5690873718a3a1527c50d
Arne Bahlo <hey@arne.me>
2024-08-19 21:41:34 +0200

    Add tab and enter mappings to cmp

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,17 +5,17 @@ local M = {}
 -- Wrap it in a function to prevent requiring this file evaluates
 -- global keymaps multiple times.
 M.init = function()
-    map("n", "<leader><space>", function()
-        require('telescope.builtin').find_files()
-    end, { silent = true, desc = "Find files" })
-    map("n", "<leader>f", function()
-        require('telescope.builtin').live_grep()
-    end, { silent = true, desc = "Live grep" })
-    map("n", "<leader>b", function()
-        require('telescope.builtin').buffers()
-    end, { silent = true, desc = "Buffers" })
+  map("n", "<leader><space>", function()
+    require('telescope.builtin').find_files()
+  end, { silent = true, desc = "Find files" })
+  map("n", "<leader>f", function()
+    require('telescope.builtin').live_grep()
+  end, { silent = true, desc = "Live grep" })
+  map("n", "<leader>b", function()
+    require('telescope.builtin').buffers()
+  end, { silent = true, desc = "Buffers" })
 
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 end
 
 return M
--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -7,6 +7,20 @@ return {
     local cmp = require('cmp')
 
     cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if #cmp.get_entries() == 1 then
+              cmp.confirm({ select = true })
+            else
+              cmp.select_next_item()
+            end
+          else
+            fallback()
+          end
+        end, { "i", "s" })
+      }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
       }),
