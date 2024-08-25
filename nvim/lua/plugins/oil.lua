c13de47163a32492c6bc4d0cbc9dd639ffa531fb
Arne Bahlo <hey@arne.me>
2024-08-25 21:36:44 +0200

    Add cokeline

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,5 @@
 return {
   'stevearc/oil.nvim',
-  -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
   dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } },
   opts = {},
 }
