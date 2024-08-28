97ff5bd5f0ecaa893e90d3fb4eedfd8ba8555b11
Arne Bahlo <hey@arne.me>
2024-08-28 22:41:09 +0200

    Add avante, delete sneak
    Let's see how this goes...

---
+++ nvim/lua/plugins/avante.lua
@@ -0,0 +1,37 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any opts here
+  },
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+        },
+      },
+    },
+    {
+      -- Make sure to setup it properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
--- nvim/lua/plugins/sneak.lua
+++
@@ -1,3 +0,0 @@
-return {
-  "justinmk/vim-sneak",
-}
